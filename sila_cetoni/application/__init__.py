2867fde0be67b9e26293f190e72356663ab30679
Florian Meinicke <florian.meinicke@cetoni.de>
2022-04-05 15:50:07 +0200

    Fix running on Linux
    when `CETONI_SDK_PATH` was set from environment variable
    In this case the `PATH`, `PYTHONPATH`, etc. variables weren't set.

--- sila_cetoni/application/__init__.py
+++ sila_cetoni/application/__init__.py
@@ -29,21 +29,22 @@ else:
                 config.CETONI_SDK_PATH = os.path.join(os.path.expanduser("~"), "CETONI_SDK")
                 print(f"Running on generic Linux - setting SDK path to {config.CETONI_SDK_PATH}")
 
-        try:
-            import qmixsdk
-        except (ModuleNotFoundError, ImportError):
-            # setup the environment for python to find the SDK and for ctypes to load the shared libs properly
-            env = os.environ.copy()
-            env["PATH"] = f"{config.CETONI_SDK_PATH}:{env['PATH']}"
-            env["PYTHONPATH"] = f"{config.CETONI_SDK_PATH}/python:{env.get('PYTHONPATH', '')}"
-            env["LD_LIBRARY_PATH"] = f"{config.CETONI_SDK_PATH}/lib:{env.get('LD_LIBRARY_PATH', '')}"
-            # uncomment the following line if you get an error like 'undefined symbol: __atomic_exchange_8'
-            # env["LD_PRELOAD"] = "/usr/lib/arm-linux-gnueabihf/libatomic.so.1.2.0"
-            # print(env)
+if platform.system() != "Windows":
+    try:
+        import qmixsdk
+    except (ModuleNotFoundError, ImportError):
+        # setup the environment for python to find the SDK and for ctypes to load the shared libs properly
+        env = os.environ.copy()
+        env["PATH"] = f"{config.CETONI_SDK_PATH}:{env['PATH']}"
+        env["PYTHONPATH"] = f"{config.CETONI_SDK_PATH}/python:{env.get('PYTHONPATH', '')}"
+        env["LD_LIBRARY_PATH"] = f"{config.CETONI_SDK_PATH}/lib:{env.get('LD_LIBRARY_PATH', '')}"
+        # uncomment the following line if you get an error like 'undefined symbol: __atomic_exchange_8'
+        # env["LD_PRELOAD"] = "/usr/lib/arm-linux-gnueabihf/libatomic.so.1.2.0"
+        # print(env)
 
-            if sys.argv[0] == "-m":
-                # started via `python -m sila_cetoni.application <args>`
-                os.execve(sys.executable, [sys.executable, "-m", __name__] + sys.argv[1:], env)
-            else:
-                # started via `sila-cetoni <args>`
-                os.execve(sys.argv[0], sys.argv, env)
+        if sys.argv[0] == "-m":
+            # started via `python -m sila_cetoni.application <args>`
+            os.execve(sys.executable, [sys.executable, "-m", __name__] + sys.argv[1:], env)
+        else:
+            # started via `sila-cetoni <args>`
+            os.execve(sys.argv[0], sys.argv, env)
